5d3dba38556c273c90e8c6a616fd6a20285cee0b
Azamat <lexoice1997@gmail.com>
2022-05-26 21:58:56 +0500

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Routs from "./routs/Routs";
+import { setupStore } from "./store/store";
+
+const store = setupStore();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Provider store={store}>
+        <Navbar />
+        <Routs />
+      </Provider>
+    </BrowserRouter>
   );
 }
 
